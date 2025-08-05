3582c5cc9f8836970cec5e08ace404fec9dbec97
Jaspreet Singh <jaspreet237@gmail.com>
2025-08-05 11:31:19 +0530

    Update message for Jenkins pipeline

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js App via GitHub Actions CI/CD\n');
+  res.end('Hello from Jenkins CI/CD\n');
 });
 
 server.listen(PORT, () => {
